8fe5aaef5a086307103a957d14d4f6de2ad0d7aa
Trường Xuân <truonghauiclck9@gmail.com>
2017-08-13 12:17:22 +0700

    fix attribute name

--- src/main/java/com/travel/service/BaseServices.java
+++ src/main/java/com/travel/service/BaseServices.java
@@ -5,9 +5,9 @@ import java.util.ArrayList;
 import antlr.collections.List;
 
 public abstract class BaseServices<T> {
-	private Class<T> objectServices;
-	public BaseServices(Class<T> obj) {
-        this.objectServices = obj;
+	private Class<T> services;
+	public BaseServices(Class<T> services) {
+        this.services = services;
     }
 	public abstract ArrayList<T> getAllObjet();
 	public abstract T getObject(Object key_get);
