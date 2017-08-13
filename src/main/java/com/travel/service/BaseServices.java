de363389b62e97e256a8edb3e4ed10bd7c822111
THIEN-NVT <THIEN-NVT@THIEN-PC>
2017-08-13 16:59:45 +0700

    +fix detail

--- src/main/java/com/travel/service/BaseServices.java
+++ src/main/java/com/travel/service/BaseServices.java
@@ -5,9 +5,9 @@ import java.util.ArrayList;
 import antlr.collections.List;
 
 public abstract class BaseServices<T> {
-	private Class<T> services;
+	private Class<T> service;
 	public BaseServices(Class<T> services) {
-        this.services = services;
+        this.service = services;
     }
 	public abstract ArrayList<T> getAllObjet();
 	public abstract T getObject(Object key_get);
