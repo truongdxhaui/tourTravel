266380ba41df2563a7034b28df47e2cc0288b8eb
Trường Xuân <truonghauiclck9@gmail.com>
2017-08-21 23:26:18 +0700

    Config Security

--- src/main/java/com/travel/config/WebSecurityConfig.java
+++ src/main/java/com/travel/config/WebSecurityConfig.java
@@ -2,6 +2,7 @@ package com.travel.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -14,29 +15,39 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	
 	@Autowired
 	private UserDetailsService userDetailsService;
-
-	@Bean
-	public UserDetailsService userDetailsService() {
-		return super.userDetailsService();
-	}
-
+	
+	
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
-
+	
 	@Autowired
-	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
-	}
-
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+    }
+	
 	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/").permitAll()
-		.antMatchers("/test").permitAll();
-
-	}
-
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .authorizeRequests()
+                .antMatchers("/").permitAll()
+                .antMatchers("/test").hasRole("LECTURE")
+                .antMatchers("/admin").hasRole("ADMIN")
+                .and()
+            .formLogin()
+            	.loginPage("/login")
+            	.usernameParameter("username")
+            	.passwordParameter("password")
+            	//.successHandler(successHandler)
+            	.failureUrl("/login?error")
+            	.and()
+        	.exceptionHandling()
+    			.accessDeniedPage("/403");
+        	
+    }
+	
 }
